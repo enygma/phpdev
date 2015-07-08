6a3be2dca87d5ea95ef06a5b2f3e96cf7d7f6c5c
Chris Cornutt <enygma@phpdeveloper.org>
2015-07-08 07:16:11 -0500

    using Commonmark in the news body output

--- lib/Phpdev/View/NewsItemView.php
+++ lib/Phpdev/View/NewsItemView.php
@@ -2,6 +2,8 @@
 
 namespace Phpdev\View;
 
+use League\CommonMark\CommonMarkConverter;
+
 class NewsItemView extends \Slim\Views\Twig
 {
 	public function render($template, $data = null)
@@ -9,17 +11,22 @@ class NewsItemView extends \Slim\Views\Twig
 		$this->setTemplatesDirectory('../templates');
 
 		$news = $data['news']->toArray();
-		if ($news['author'] === 'enygma') {
+		if (isset($news['author']) && $news['author'] === 'enygma') {
 			$news['author'] = 'Chris Cornutt';
 		}
-		$news['date'] = date('M d, Y @ H:i:s', $news['date']);
-		$news['title'] = stripslashes($news['title']);
-		$news['story'] = stripslashes($news['story']);
+		$news['date'] = (isset($news['date'])) ? date('M d, Y @ H:i:s', $news['date']) : '';
+		$news['title'] = (isset($news['title'])) ? stripslashes($news['title']) : '';
+
+		$converter = new CommonMarkConverter();
+		$news['story'] = (isset($news['story'])) ? stripslashes($news['story']) : '';
+		$news['story'] = $converter->convertToHtml($news['story']);
 
 		// Find the first colon and add a <br>
 		$first = strpos($news['title'], ':');
-		$news['title'] = substr($news['title'], 0, $first+1).'<br/>'
-			.substr($news['title'], $first+1);
+		if ($first !== false) {
+			$news['title'] = substr($news['title'], 0, $first+1).'<br/>'
+				.substr($news['title'], $first+1);
+		}
 
 		$data['news'] = array($news);
 
