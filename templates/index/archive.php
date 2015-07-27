ae9fb278cf2c4f96d5181adeb19f503a7c9718f6
Chris Cornutt <enygma@phpdeveloper.org>
2015-07-27 12:47:27 -0500

    fixing archive template to better align dates

--- templates/index/archive.php
+++ templates/index/archive.php
@@ -10,7 +10,7 @@
 	</tr>
 	{% for item in day %}
 	<tr>
-		<td valign="top" class="link_date">@ {{ item.date|date('H:i:s') }}</td>
+		<td valign="top" class="link_date">@{{ item.date|date('H:i:s') }}</td>
 		<td width="16" valign="top"><img src="/img/layout/gray_arrows.gif"/></td>
 		<td align="left"><a class="archive_link" href="/news/{{ item.id }}">{{ item.title }}</a></td>
 	</tr>
