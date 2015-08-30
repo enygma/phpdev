4801afa77720cefb48a67eecab25379d563aa823
Chris Cornutt <ccornutt@phpdeveloper.org>
2015-08-30 09:12:18 -0500

    updating news controller for more effective caching

--- controller/news.php
+++ controller/news.php
@@ -1,15 +1,18 @@
 <?php
+use Mailgun\Mailgun;
 
 $app->group('/news', function() use ($app, $view) {
 
 	$app->get('/:id', function($itemId) use ($app, $view) {
 		$newsData = $app->di['cache']->get('news:'.$itemId);
+        $news = new \Phpdev\Model\News($app->di['db']);
+        $news->addViewCount($itemId);
 
 		if ($newsData === null) {
-			$news = new \Phpdev\Model\News($app->di['db']);
+			//$news = new \Phpdev\Model\News($app->di['db']);
 			$news->findById($itemId);
 			$pending = ($news->date <= time()) ? false : true;
-			$news->addViewCount();
+			//$news->addViewCount();
 
 			$data = array(
 				'news' => $news,
@@ -27,10 +30,10 @@ $app->group('/news', function() use ($app, $view) {
 		} else {
 			$renderedNews = $newsData;
 		}
+        $data = ['item' => $renderedNews];
 
-		echo $view->render('news/view.php', array(
-			'item' => $renderedNews
-		));
+		echo $view->render('news/view.php', $data);
+
 	})->conditions(array('id' => '[0-9]+'));
 
 	$app->get('/submit', function() use ($app, $view) {
@@ -39,11 +42,39 @@ $app->group('/news', function() use ($app, $view) {
 	$app->post('/submit', function() use ($app, $view) {
 		$data = $app->request->post();
 
+        $mg = new Mailgun('key-bea3f09c427ef010a33c2710aebeae98');
+        $domain = 'phpdeveloper.org';
+        $domain = 'sandbox007d6d6814314253851907a50f146a99.mailgun.org';
+
+        $denyRegex = [
+            '/176\.10\.104/', '/188\.143\.232/', '/120\.40\.144\.210/',
+            '/27\.153\.156\.165/', '/46\.119\.115/', '/76\.164\.202/',
+            '/142\.54\.172/', '/120\.43\.27/'
+        ];
+        $match = false;
+        foreach ($denyRegex as $regex) {
+            $find = preg_match($regex, $_SERVER['REMOTE_ADDR']);
+            if ($match == false && $find == 1) {
+                $match = true;
+            }
+        }
+
+        if ($match == false){
+        $mg->sendMessage($domain, [
+            'from' => 'commentMonitor@phpdeveloper.org',
+            'to' => 'enygma@phpdeveloper.org',
+            'subject' => 'News Submission: '.$data['title'],
+            'text' => 'From: '.$data['name']."\n\nStory:\n".$data['story']."\n\nIP address: ".$_SERVER['REMOTE_ADDR']."\n\n"
+        ]);
+        }
+
 		// Send the email with the content
-		$subject = 'News Submission: '.$data['title'];
+		/*
+        $subject = 'News Submission: '.$data['title'];
 		$body = 'From: '.$data['name']."\n\nStory:\n".$data['story']."\n\n";
 
 		mail('enygma@phpdeveloper.org', $subject, $body, "From: commentMonitor@phpdeveloper.org\r\n");
+        */
 		echo $view->render(
 			'news/submit.php',
 			array('success' => true)
